3ec96f10287cb5d58fa95a909366886d4c88e9b8
jsubercaze <julien.subercaze@univ-st-etienne.fr>
2016-02-10 16:44:05 +0100

    Adding Infobox Parsing and First Paragraph Extraction

--- src/main/java/edu/jhu/nlp/wikipedia/InfoBox.java
+++ src/main/java/edu/jhu/nlp/wikipedia/InfoBox.java
@@ -1,15 +1,40 @@
 package edu.jhu.nlp.wikipedia;
 
+import java.util.HashMap;
+
 /**
  * A class abstracting Wiki infobox
+ * 
  * @author Delip Rao
  */
 public class InfoBox {
-  String infoBoxWikiText = null;
-  InfoBox(String infoBoxWikiText) {
-    this.infoBoxWikiText = infoBoxWikiText;
-  }
-  public String dumpRaw() {
-    return infoBoxWikiText;
-  }
+	String infoBoxWikiText = null;
+	HashMap<String, String> boxMap;
+
+	InfoBox(String infoBoxWikiText) {
+		this.infoBoxWikiText = infoBoxWikiText;
+		String[] split = infoBoxWikiText.split("\\n");
+		boxMap = new HashMap<>(split.length);
+		for (String string : split) {
+			String[] split2 = string.split("=", 2);
+			if (split2.length != 2)
+				continue;
+			String key = split2[0].trim().substring(1);
+
+			String value = split2[1].trim();
+			if (value.length() > 0) {
+				boxMap.put(key, value);
+			}
+		}
+	}
+
+	public String dumpRaw() {
+		return infoBoxWikiText;
+	}
+
+	@Override
+	public String toString() {
+		return "InfoBox [infoBoxWikiText=" + infoBoxWikiText + ", boxMap=" + boxMap + "]";
+	}
+
 }
